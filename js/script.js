04f590559c1df88577424ed47e1a845a9d55a039
YohnSuh <yohnsuh@gmail.com>
2023-11-20 19:07:37 +0900

    footer 확인

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 $('.hamburger').click(function() {
     $('.gnb').toggleClass('active');
 });
+
+$('footer h3').click(function() {
+    $(this).toggleClass('toggle');
+    $(this).next().stop().slideToggle();
+    $(this).parent().siblings().find('.content').stop().slideUp();
+});
