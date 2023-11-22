983bd1484a9e47b9d260d2d2a1c1bfb07ca9ea76
YohnSuh <yohnsuh@gmail.com>
2023-11-22 18:17:07 +0900

    배너 슬라이드 페이지 표시하기

--- js/script.js
+++ js/script.js
@@ -5,5 +5,7 @@ $('.hamburger').click(function() {
 $('footer h3').click(function() {
     $(this).toggleClass('toggle');
     $(this).next().stop().slideToggle();
+    $(this).parent().siblings().find('h3').removeClass('toggle');
     $(this).parent().siblings().find('.content').stop().slideUp();
 });
+
--- js/swiper.js
+++ js/swiper.js
@@ -2,3 +2,28 @@ let banner = new Swiper('.bannerSlide', {
     loop : true,
     autoplay: true
 });
+
+/* 서비스 슬라이드; 모바일에서만 슬라이드 생기게 */
+let ww = $(window).width();
+let menuSwiper = undefined;
+
+function initSwiper() {
+    if(ww <= 992 && menuSwiper == undefined) {
+        menuSwiper = new Swiper('.serviceSlide', {
+            loop: true,
+            loopAdditionalSlides : 1,
+            slidesPerView : 2,
+            autoplay : true
+        });
+    } else if(ww > 992 && menuSwiper != undefined) {
+        menuSwiper.destroy();
+        menuSwiper = undefined;
+    }
+};
+
+initSwiper();
+
+$(window).on('resize', function() {
+    ww = $(window).width();
+    initSwiper();
+});
