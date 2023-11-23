58390b6b42d9e6419fa0811a764b4717749e2453
YohnSuh <yohnsuh@gmail.com>
2023-11-23 13:04:10 +0900

    우리은행 완성

--- js/swiper.js
+++ js/swiper.js
@@ -1,6 +1,7 @@
 let banner = new Swiper('.bannerSlide', {
     loop : true,
-    autoplay: true
+    autoplay: {disableOnInteraction: false},
+    pagination : {el : '.swiper-pagination', clickable : true},
 });
 
 /* 서비스 슬라이드; 모바일에서만 슬라이드 생기게 */
@@ -10,10 +11,12 @@ let menuSwiper = undefined;
 function initSwiper() {
     if(ww <= 992 && menuSwiper == undefined) {
         menuSwiper = new Swiper('.serviceSlide', {
-            loop: true,
-            loopAdditionalSlides : 1,
             slidesPerView : 2,
-            autoplay : true
+            autoplay : true,
+            scrollbar:{
+                el : '.swiper-scrollbar',
+                draggable: true
+            }
         });
     } else if(ww > 992 && menuSwiper != undefined) {
         menuSwiper.destroy();
